828d332498dd51558014342a067094c8f6943ea6
Paolino <paolo.veronelli@com>
2015-10-16 12:40:21 +0200

    resolved space leak with strictness annotations and a strictList function

--- Main.hs
+++ Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ViewPatterns, Rank2Types, ExistentialQuantification, TemplateHaskell#-}
+{-# LANGUAGE ViewPatterns, Rank2Types, ExistentialQuantification, TemplateHaskell, BangPatterns#-}
 
 import Prelude hiding (minimum, maximum)
 import Data.List (unfoldr)
@@ -8,7 +8,7 @@ import Control.Exception
 import Control.Monad
 import System.IO.Error
 import Control.Arrow
-import qualified Data.Map as M
+import qualified Data.Map.Strict as M
 import Control.Lens.TH
 import Control.Lens
 
@@ -17,24 +17,30 @@ import Data.List.Split
 
 
 
+----------------------------------------------
+---- forcing a list evaluation with elements
+----------------------------------------------
+
+strictList xs = strictList' xs `seq` xs where
+  strictList' [] = ()
+  strictList' (x:xs) = x `seq` strictList' xs
+
+
 -------------------------------------------------
 --- stateful operation type ----------------------
 -----------------------------------------------
 
 -- iso to a mealy machine (Category instance)
 -- (https://github.com/paolino/sensors/blob/master/MealyT.hs)
--- Nothing is for null row fields, a is input, b is output
-data Operation b a = Operation b (Maybe a -> Operation b a) 
+-- Nothing is for null row fields, a is input, b is strict output
+data Operation b a = Operation !b (Maybe a -> Operation b a) 
 
---  a box for operations hiding result type, as we only need to report. We leave a as a parameter to make some spaghetti code later :(
-data Machine a = forall b . (Show b) => Machine (Operation b a)
+--  a box for operations hiding result type, as we only need to report. Strict in the Operation to force output
+data Machine a = forall b . (Show b) => Machine !(Operation b a)
 
 -- step the machine, feeding an input
 operate ::  Maybe a -> Machine a -> Machine a
 operate z (Machine (Operation x f)) = Machine .  f  $ z
-
--- couldn't find out the right strictness annotation
-force (Machine (Operation r _)) = r `seq` return ()
 
 ------------------------------------------
 --- machines ----------------------- 
@@ -42,7 +48,7 @@ force (Machine (Operation r _)) = r `seq` return ()
 
 -- count nulls and hits
 -- if Machine hides the input type 'a' requesting a Read counter should nail down 'a' to some fake Readable datatype detecting null
-data Counter = Counter {nulls :: Int, hit :: Int, totals :: Int} deriving Show
+data Counter = Counter {nulls :: !Int, hit :: !Int, totals :: !Int} deriving Show
 
 counter  :: Operation Counter a
 counter = let 
@@ -54,13 +60,13 @@ counter = let
 -----------------------------------------
 
 -- track minimum value
-data Minimum = Minimum (Maybe Float) deriving Show
+data Minimum = Minimum !(Maybe Float) deriving Show
 
 minimum :: Operation Minimum Float
 minimum = let 
   make m = Operation (Minimum m) $ f m 
   f Nothing x = make x
-  f (Just m) (Just x) 
+  f (Just !m) (Just x) 
     | x < m = make $ Just x
     | otherwise = make $ Just m
   f m Nothing = make m
@@ -68,13 +74,13 @@ minimum = let
 ---------------------------------------------
 
 -- track maximum
-data Maximum = Maximum (Maybe Float) deriving Show
+data Maximum = Maximum !(Maybe Float) deriving Show
 
 maximum :: Operation Maximum Float
 maximum = let 
   make m = Operation (Maximum m) $ f m 
   f Nothing x = make x
-  f (Just m) (Just x) 
+  f (Just !m) (Just x) 
     | x > m = make $ Just x
     | otherwise = make $ Just m
   f m Nothing = make m
@@ -86,10 +92,10 @@ data Average = Average (Maybe Float) deriving Show
 
 average :: Operation Average Float
 average = let
-  make n a = Operation (Average $ (/ fromIntegral n) `fmap` a ) $ f n a
+  make !n a = Operation (Average $ (/ fromIntegral n) `fmap` a ) $ f n a
   f n Nothing (Just x) = make (n + 1) (Just x)
   f n x Nothing = make n x
-  f n (Just a) (Just x) = make (n + 1) (Just $ a + x)
+  f n (Just !a) (Just x) = make (n + 1) (Just $ a + x)
   in make 0 Nothing
     
 ----------------------------------------------
@@ -109,15 +115,15 @@ instance Read Chars where
 
 -- traverse statistic and compute the Occurs 
 report :: M.Map String Int -> Occurs
-report = M.foldrWithKey g NoOccurs  where
-      g key count NoOccurs =  Occurs (Occur key count) (Occur key count)
-      g key count o@(Occurs (Occur kmi mi) (Occur kma ma))
-        | count > ma =   Occurs (Occur kmi mi) (Occur key count)
-        | count == ma && kma < key =  Occurs (Occur kmi mi) (Occur key count)
-        | count < mi =  Occurs (Occur key count) (Occur kma ma)
-        | count == mi && kmi > key =  Occurs (Occur key count) (Occur kma ma)
+report m = r `seq` M.foldrWithKey g NoOccurs m where
+      g key count NoOccurs =  Occurs (Occur key count) (Occur key count) 
+      g key count o@(Occurs (Occur kmi mi) (Occur kma ma) )
+        | count > ma =   Occurs (Occur kmi mi) (Occur key count) 
+        | count == ma && kma < key =  Occurs (Occur kmi mi) (Occur key count) 
+        | count < mi =  Occurs (Occur key count) (Occur kma ma) 
+        | count == mi && kmi > key =  Occurs (Occur key count) (Occur kma ma) 
         | otherwise = o
-
+      (_,r) = if M.size m > 0 then M.findMin m else (undefined,0)
 occurs :: Operation Occurs Chars
 occurs = let
   make m  = Operation (report m) $ f m 
@@ -128,8 +134,8 @@ occurs = let
 ----------------------------------------------
 
 -- track legths 
-data Length = Length String Int deriving Show
-data Lengths = NoLengths | Lengths {smallestL::Length, biggestL ::Length} deriving Show
+data Length = Length !String !Int deriving Show
+data Lengths = NoLengths | Lengths {smallestL :: !Length, biggestL :: !Length} deriving Show
 
 
 lengths :: Operation Lengths Chars
@@ -148,44 +154,46 @@ lengths = let
                 
 --------------------------------------
 -- track average length (sum)
-data AverageLength = AverageLength (Maybe Float) deriving Show
+data AverageLength = AverageLength !(Maybe Float) deriving Show
 
 averageLength :: Operation AverageLength Chars
 averageLength = let
-  make n a = Operation (AverageLength $ (\x -> fromIntegral x / fromIntegral n ) `fmap` a ) $ f n a
+  make !n a = Operation (AverageLength $ (\x -> fromIntegral x / fromIntegral n ) `fmap` a ) $ f n a
   f n Nothing (Just (Chars x)) = make (n + 1) (Just $ length x)
   f n x Nothing = make n x
-  f n (Just a) (Just (Chars x)) = make (n + 1) (Just $ a + length x)
+  f n (Just !a) (Just (Chars x)) = make (n + 1) (Just $ a + length x)
   in make 0 Nothing
 
 -------------------------------------------------------------------
 --- end of machines ------------------------------------------
 ---------------------------------------------------------------
 
-data Parallels = forall a. Read a => Parallels [Machine a]
+--- a  contatiner for machines reading same input, should have a strict list as argument, so we force the spine and leave the element evaluation to stepParallels
+data Parallels = forall a. Read a => Parallels ![Machine a]
 
--- step all machines of a Parallels with same input
+-- step all machines of a Parallels with same input, forcing Machines evaluation
 stepParallels :: String -> Parallels -> Parallels
-stepParallels s (Parallels ms) = Parallels $ map (operate . readMaybe $ s) ms
+stepParallels s (Parallels ms) = Parallels $  strictList $ map (operate . readMaybe $ s) ms
 
 
 -- a type for different columns
--- the set of machines are intended to work in parallel, thus they share input type
+-- the set of machines are intended to work in parallel, thus they share input type, strict in the _machine field
 data Column  
-  = Numeric {title :: String , _machines :: Parallels} 
-  | Textual {title :: String,  _machines :: Parallels} 
+  = Numeric {title :: String , _machines :: !Parallels} 
+  | Textual {title :: String,  _machines :: !Parallels} 
 
 makeLenses ''Column
 
 
 
-forceColumn (view machines -> Parallels ms) = mapM_ force ms -- this must be investigated, without it blows memory
 
 -- from comma separated strings to Column
 parseColumns :: [String] -> [Column]
 parseColumns = map (readField . break (== ' '))  where
-  readField (tail -> name, init -> " (number)") = Numeric name . Parallels $ [Machine counter, Machine minimum, Machine maximum, Machine average]
-  readField (tail -> name, init -> " (text)")   = Textual name . Parallels $ [Machine counter, Machine occurs, Machine lengths, Machine averageLength]
+  -- readField (tail -> name, init -> " (number)") = Numeric name . Parallels $ [Machine counter :: Machine Float , Machine minimum, Machine maximum, Machine average]
+  readField (tail -> name, init -> " (number)") = Numeric name . Parallels $ [Machine counter :: Machine Float, Machine minimum, Machine maximum, Machine average ]
+  readField (tail -> name, init -> " (text)")   = Textual name . Parallels $ [Machine counter :: Machine Chars , Machine lengths, Machine averageLength, Machine occurs]
+  -- readField (tail -> name, init -> " (text)")   = Textual name . Parallels $ [Machine counter :: Machine Chars , Machine occurs, Machine lengths, Machine averageLength]
 
 
 -- print the values of the machines 
@@ -200,18 +208,17 @@ output h = do
 
 -- feed a line of values to the machines
 feed :: [String] -> [Column] ->  [Column]
-feed  = zipWith (over machines .stepParallels) 
+feed  ss cs =   zipWith (over machines . stepParallels)  ss cs 
+  
 
 -- get a line without return carriage
 csv :: IO [String]
 csv = splitOn "," <$> filter (/= '\r') <$> getLine
 
 
--- read all next lines updating the column list on each turn. Return last update
+-- read all next lines updating the column list on each turn. Return last update, force each Column and the list to whnf 
 cycling :: [Column] -> IO [Column]
-cycling cs = do 
-  mapM_ forceColumn cs -- problem with lazyness
-  tryJust (guard . isEOFError) csv >>= either (const $ return cs) (cycling . flip feed cs)
+cycling !(strictList -> cs) = tryJust (guard . isEOFError) csv >>= either (const $ return cs) (cycling . flip feed cs)
 
 -- read first line to produce the booting [Column] value, cycle by reading line by line and print a report of the result
 main = parseColumns <$> csv >>= cycling >>= mapM_ output
