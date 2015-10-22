05740a278e92af67cee28f97d9bc11f3e43d4bee
Paolino <paolo.veronelli@com>
2015-10-22 19:06:53 +0200

    multifeed, single machine threads, more speedup even on 1 core

--- Core.hs
+++ Core.hs
@@ -13,6 +13,7 @@ import Control.Lens
 import Control.Lens.TH
 import Lib (forceElems, zipWithU, forceElemsPar)
 import Text.Read (readMaybe)
+import Data.List
 
 -- final filter boxes are Moore machines 
 -- (which is not a Category instance :-()
@@ -28,12 +29,19 @@ data Machine a = forall b . Show b => Machine (Moore a b)
 operate ::  Machine a -> a -> Machine a
 operate (Machine (Moore !x f)) = Machine .  f  
 
+multioperate :: Machine a -> [a] -> Machine a
+multioperate (Machine m@(Moore !x _)) = Machine . foldl' k m where
+  k (Moore x f) = f 
 --- a  contatiner for machines reading same input, should have a strict list as argument, so we force the spine and leave the element evaluation to stepParallels
 data Parallels = forall a. Read a => Parallels [Machine (Maybe a)]
 
 -- step all machines of a Parallels with same input , parsed, forcing each Machine evaluation
 step :: String -> Parallels -> Parallels
 step s (Parallels !(forceElems -> ms)) = Parallels $ map ( flip operate . readMaybe $ s) ms
+
+multistep :: [String] -> Parallels -> Parallels
+multistep xs (Parallels !(forceElemsPar -> ms)) = Parallels $ map (flip multioperate xs') ms
+  where xs' = map readMaybe $ xs
 
 -- the set of machines for each column are intended to work in parallel
 data Column  = Column {title :: String , _machines :: !Parallels} 
@@ -44,4 +52,5 @@ makeLenses ''Column
 feed :: [String] -> [Column] ->  [Column]
 feed  = zipWithU (over machines . step)  
 
-
+multifeed :: [[String]] -> [Column] -> [Column]
+multifeed xs = zipWithU (over machines . multistep) (transpose xs)
--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE 
     ViewPatterns
   , BangPatterns
+  , ScopedTypeVariables
   #-}
 
 import Prelude hiding (minimum, maximum)
@@ -8,7 +9,7 @@ import Control.Exception (tryJust)
 import Control.Monad (guard,forM_)
 import System.IO.Error (isEOFError)
 import System.Environment (getArgs)
-import Control.Lens (view)
+import Control.Lens (view,over,_1)
 import Text.Read (readMaybe)
 import Data.Maybe (listToMaybe, fromMaybe)
 
@@ -16,7 +17,7 @@ import Data.Maybe (listToMaybe, fromMaybe)
 import Data.List.Split (splitOn)
 
 import Data.Machine.Moore (Moore (..))
-import Core (operate, Machine (..), Parallels (..), Column (..), feed, machines)
+import Core (operate, Machine (..), Parallels (..), Column (..), feed, machines, multifeed)
 import Lib (forceElemsPar, forceElems)
 import Machines (minimum, maximum, counter, averageLength, average, lengths, occurs)
 import Control.Monad
@@ -56,7 +57,24 @@ cycling nc n !cs = let
   in tryJust (guard . isEOFError) csv >>=   
      either (const $ return cs) ( cycling nc (n + 1). flip feed cs')
 
+multicycling :: Int -> [Column] -> IO [Column]
+multicycling nc !cs = do
+  let untilEOF n 
+                | n > nc = return ([],False)
+                | otherwise = do
+                    line <- tryJust (guard . isEOFError) csv
+                    case line of 
+                        Left _ -> return ([],True)
+                        Right x -> over _1 (x:) <$> untilEOF (n + 1)
+  (ls :: [[String]],out) <- untilEOF 0
+  let cs' = forceElemsPar (multifeed ls cs)
+  if null ls then return cs 
+    else if out then return cs'
+      else multicycling nc cs'
+  
+
+
 -- read first line to produce the booting [Column] value, cycle by reading line by line and print a report of the result
 main = do
   nc <- (fromMaybe 1 . (listToMaybe  >=> readMaybe)) <$>  getArgs
-  map column <$> csv >>= cycling nc 0 >>= mapM_ output
+  map column <$> csv >>= multicycling nc >>= mapM_ output
